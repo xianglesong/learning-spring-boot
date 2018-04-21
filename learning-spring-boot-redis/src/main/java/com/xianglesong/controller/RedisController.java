be2f0fdbe901ce9cccaf1d8115a62caec1c7cc8f
xianglesong <rulinma@qq.com>
2018-04-21 17:59:26 +0800

    redis

---
+++ learning-spring-boot-redis/src/main/java/com/xianglesong/controller/RedisController.java
@@ -0,0 +1,34 @@
+package com.xianglesong.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xianglesong.dao.RedisDao;
+
+@RestController
+public class RedisController {
+
+  public static final Logger logger = LoggerFactory.getLogger(RedisController.class);
+
+  @Autowired
+  RedisDao redisDao;
+
+
+  @RequestMapping(value = "/api/redis", method = RequestMethod.GET)
+  public String redisTest(HttpServletRequest request, HttpServletResponse response) {
+    String key = request.getParameter("key");
+    System.out.println(System.currentTimeMillis());
+    
+    return redisDao.getValue(key);
+  }
+
+}
